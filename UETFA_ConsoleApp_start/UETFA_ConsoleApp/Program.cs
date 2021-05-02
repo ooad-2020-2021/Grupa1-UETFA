b5c2e945f1cdd21020db2aba5c553846295e4e0a
iicanovic1 <73068169+iicanovic1@users.noreply.github.com>
2021-05-02 16:58:40 +0200

    blank

--- UETFA_ConsoleApp_start/UETFA_ConsoleApp/Program.cs
+++ UETFA_ConsoleApp_start/UETFA_ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace UETFA_ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World2!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
