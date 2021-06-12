54d4e3b5ea959cac6bf54c72cfce1986b40351d1
nspahic3 <nspahic3@etf.unsa.ba>
2021-06-12 15:28:11 +0200

    Dodan Postani Premium

---
+++ UETFA/UETFA/Controllers/PremiumsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using UETFA.Data;
+using UETFA.Models;
+
+namespace UETFA.Controllers
+{
+    public class PremiumsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PremiumsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult PogledajPogodnosti()
+        {
+            return View();
+        }
+
+        // GET: Premiums
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Premium.ToListAsync());
+        }
+
+        // GET: Premiums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var premium = await _context.Premium
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (premium == null)
+            {
+                return NotFound();
+            }
+
+            return View(premium);
+        }
+
+        // GET: Premiums/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Premiums/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,imeVlasnikaKartice,brojKartice,cvc,datum")] Premium premium)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(premium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(premium);
+        }
+
+        // GET: Premiums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var premium = await _context.Premium.FindAsync(id);
+            if (premium == null)
+            {
+                return NotFound();
+            }
+            return View(premium);
+        }
+
+        // POST: Premiums/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,imeVlasnikaKartice,brojKartice,cvc,datum")] Premium premium)
+        {
+            if (id != premium.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(premium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PremiumExists(premium.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(premium);
+        }
+
+        // GET: Premiums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var premium = await _context.Premium
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (premium == null)
+            {
+                return NotFound();
+            }
+
+            return View(premium);
+        }
+
+        // POST: Premiums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var premium = await _context.Premium.FindAsync(id);
+            _context.Premium.Remove(premium);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PremiumExists(int id)
+        {
+            return _context.Premium.Any(e => e.ID == id);
+        }
+    }
+}
--- UETFA/UETFA/Models/Premium.cs
+++ UETFA/UETFA/Models/Premium.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace UETFA.Models
     public class Premium
     {
         public int ID{ get; set; }
+        [DisplayName("Ime")]
         public string imeVlasnikaKartice { get; set; }
+        [DisplayName("Broj kreditne kartice")]
         public int brojKartice { get; set; }
+        [DisplayName("Sigurnosni kod(CVV)")]
         public int cvc { get; set; }
+        [DisplayName("Datum isteka kartice")]
         public DateTime datum { get; set; }
     }
 }
